e75f3cc1453145a3d7209ace3cb041f67752e511
Kazashim <kazashimkuzasuwat@gmail.com>
2019-06-18 14:54:26 +0200

    code change

--- include/nav_bar.php
+++ include/nav_bar.php
@@ -1,8 +1,12 @@
- <!-- main content area start -->
- <div id="preloader">
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
         <div class="loader"></div>
-    </div> 
- <div class="main-content">
+    </div>
+    <!-- preloader area end -->
             <!-- header area start -->
             <div class="header-area">
                 <div class="row align-items-center">

2b828938d5a27e7645aa5b2fca2a64d42b2a71d4
Kazashim <kazashimkuzasuwat@gmail.com>
2019-06-19 12:01:54 +0200

    Update login.php

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
                             <div class="row">
                                
                              <div class="col-lg-2 col-sm-4 col-xs-12">
-                                <button class="btn btn-block btn-outline btn-rounded btn-warning">Warning</button>
+                                <a href="https://app.cynojine.com/oauth?app_id='60e42b3ba620d4573388' button class="btn btn-block btn-outline btn-rounded btn-warning">login</button>
                                 </div>
 </div>
 </div>
@@ -16,3 +16,4 @@
                         </div></center>
                   
                 <!--row -->
+                <a href="<?php echo $music->config->wowonder_domain_uri.'/oauth?app_id='.$music->config->wowonder_app_ID;?>" class="btn_social btn_gp bold" style="background-color: #212121;color: #fff;"><span class="btn_wo"><img src="<?php echo $music->config->wowonder_img;?>" style="width: 20px;"></span> {{LANG Login with Wowonder}}</a>

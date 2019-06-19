2e4b5770564c1d665cedba90dbc39f753bc75b94
Kazashim <kazashimkuzasuwat@gmail.com>
2019-06-19 11:56:36 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php require_once ('index.php'); ?>
+<div class="page-wrapper">
+            <div class="container-fluid">
 <center><div class="row">
                     
                           <center>  <h3 class="m-b-0 m-t-30 box-title">Login with cynojine </h3></center>
@@ -8,6 +10,8 @@
                              <div class="col-lg-2 col-sm-4 col-xs-12">
                                 <button class="btn btn-block btn-outline btn-rounded btn-warning">Warning</button>
                                 </div>
+</div>
+</div>
                             </div>
                         </div></center>
                   

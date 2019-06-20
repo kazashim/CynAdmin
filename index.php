11b0aaca266687de8495076e1ad5e80a509c9a02
Kazashim <kazashimkuzasuwat@gmail.com>
2019-06-20 02:09:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php require_once ('include/header.php');
       require_once ('include/nav_bar.php'); 
-      
       ?>
-
 <?php require_once ('include/side_bar.php'); ?>
 <?php require_once ('include/footer.php'); ?>
